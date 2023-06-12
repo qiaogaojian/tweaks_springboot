77d5baa7eb7eb230dc3a889af8b1b58117867ebf
qiaogaojian <qiaogaojian@vip.qq.com>
2023-06-12 17:20:42 +0800

    del: 删除不用代码

--- src/main/java/com/mega/tweaks/TweaksApplication.java
+++ src/main/java/com/mega/tweaks/TweaksApplication.java
@@ -10,20 +10,7 @@ import java.util.Arrays;
 @SpringBootApplication
 public class TweaksApplication
 {
-
     public static void main(String[] args) {
         SpringApplication.run(TweaksApplication.class, args);
-    }
-
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return (args) -> {
-            System.out.println("Let's inspect the bean provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-        };
     }
 }
