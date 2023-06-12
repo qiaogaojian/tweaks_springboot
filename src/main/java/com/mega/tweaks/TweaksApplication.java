4818bb7034c9585b33136c7016760009bfe4b367
qiaogaojian <qiaogaojian@vip.qq.com>
2023-06-12 13:39:21 +0800

    init project

---
+++ src/main/java/com/mega/tweaks/TweaksApplication.java
@@ -0,0 +1,29 @@
+package com.mega.tweaks;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class TweaksApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(TweaksApplication.class, args);
+    }
+
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return (args) -> {
+            System.out.println("Let's inspect the bean provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
+    }
+}
---
+++ src/test/java/com/mega/tweaks/TweaksApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mega.tweaks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweaksApplicationTests
+{
+
+    @Test
+    void contextLoads() {
+    }
+
+}
