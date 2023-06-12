483c94aa434c2bc84463ef7b064eacc3c93d6b79
qiaogaojian <qiaogaojian@vip.qq.com>
2023-06-12 16:08:57 +0800

    config: 配置 swagger

--- src/main/java/com/mega/tweaks/TweaksApplication.java
+++ src/main/java/com/mega/tweaks/TweaksApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableWebMvc
 public class TweaksApplication
 {
 
---
+++ src/main/java/com/mega/tweaks/api/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.mega.tweaks.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.swagger.annotations.ApiOperation;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Profile({"dev", "test"})
+@EnableOpenApi
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer
+{
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .apiInfo(new ApiInfoBuilder()
+                                 .title("Tweaks")
+                                 .version("1.0")
+                                 .build())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
