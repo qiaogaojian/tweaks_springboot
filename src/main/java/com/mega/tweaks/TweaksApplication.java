6cf979ab963370dc72f50644633cb8e93a14b325
qiaogaojian <qiaogaojian@vip.qq.com>
2023-06-12 17:22:46 +0800

    del: 删除不用引用

--- src/main/java/com/mega/tweaks/TweaksApplication.java
+++ src/main/java/com/mega/tweaks/TweaksApplication.java
@@ -1,11 +1,7 @@
 package com.mega.tweaks;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class TweaksApplication
